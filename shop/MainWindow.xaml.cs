27219c375a6eb955cccbf81bf887af4728c316e9
Rasimqa <gizatullinunga200@gmail.com>
2024-04-27 12:58:12 +0300

    работает только грибная а еще листа нет

--- shop/MainWindow.xaml.cs
+++ shop/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
 
 namespace shop
 {
@@ -23,15 +24,30 @@ namespace shop
         public MainWindow()
         {
             InitializeComponent();
-            MRGT.Text = 
+        }
+        List<string> pizza = new List<string>() { "Margarita" };
+        List<string> pizzacard = new List<string>() { "Gribnaya", "Myasnaya", "Cheese", "Foursezone" };
 
-        class shop
+        private void Button_Click_AddCard(object sender, RoutedEventArgs e)
         {
-            List<string> Pizza = new List<string>() { "Margarita" };
-
+            if (AddPiz.Text == "Gribnaya")
+            {
+                pizza1.Opacity = 1;
+                pizza1_name.Opacity = 1;
+                pizza1_price.Opacity = 1;
+                pizza1_text.Opacity = 1;
+                IMG1.Opacity = 1;
+                IMG1.Source = new BitmapImage(new Uri("/IMG/gribi.png", UriKind.Relative));
+                pizza1_name.Text = "Gribnaya";
+                pizza1_price.Text = "Price: 14$\r\n";
+                pizza1_text.Text = "This good pizza";
+            }
             
         }
 
-            
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
